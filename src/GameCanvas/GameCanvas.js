45acc6408ea15afaa3cbe0bdecfa100e219c61a7
Anton Anderzén <anderze@kth.se>
2020-05-06 01:54:19 +0200

    Highligthing current grid.

--- src/GameCanvas/GameCanvas.js
+++ src/GameCanvas/GameCanvas.js
@@ -4,39 +4,56 @@ import {GameConstants} from '../GameEngine/GameConstants'
 export default class GameCanvas extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {
-            gridSize: GameConstants.SPRITE_SIZE
-        }
+        this.handleMouseMove = this.handleMouseMove.bind(this);
+
     }
   
   
     componentWillMount(){
         this.setState({
             canvasSize: { canvasWidth: GameConstants.MAP_WIDTH,
-                         canvasHeight: GameConstants.MAP_HEIGHT}
+                         canvasHeight: GameConstants.MAP_HEIGHT},
+                         gridSize: GameConstants.SPRITE_SIZE
         })
     }
     componentDidMount(){
         const{ canvasWidth, canvasHeight } = this.state.canvasSize;
         this.canvasGrid.width = canvasWidth ;
         this.canvasGrid.height = canvasHeight; 
+        this.canvasCoordinates.width = canvasWidth ;
+        this.canvasCoordinates.height = canvasHeight; 
+        this.getCanvasPosition(this.canvasCoordinates)
         this.drawGrids()
     }
-
+    shouldComponentUpdate(nextProps, nextState){
+        if(nextState.currentGrid !== this.state.currentGrid){
+            const {q, r, x, y} = nextState.currentGrid;
+            console.log(q,r,x,y)
+            const {canvasWidth, canvasHeight} = this.state.canvasSize;
+            const ctx = this.canvasCoordinates.getContext("2d");
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+            this.drawGrid(this.canvasCoordinates, this.Point(x,y), "lime", 3);
+            return true;
+        }
+        return false;
+    }
     drawGrids(){
+            console.log(this.state.gridSize)
         for (let r = 0; r <= this.state.canvasSize.canvasHeight/this.state.gridSize; r++) {
             for (let q = 0; q <= this.state.canvasSize.canvasWidth/this.state.gridSize; q++) {
                 let center = this.gridToPixel(this.Grid(q, r))
                 this.drawGrid(this.canvasGrid, center)
+                this.canvasGrid.getContext("2d").fillText(r, center.x+10, center.y);
+                this.canvasGrid.getContext("2d").fillText(q, center.x-10, center.y);
             }
         }
     }
 
-    drawGrid(canvasID, center){
+    drawGrid(canvasID, center, color="black", width=1){
         for(let i = 0; i < 4; i++){
             const start = this.getGridCornerCoord(center, i);
             const end = this.getGridCornerCoord(center, i+1);
-            this.drawLine(canvasID, {x: start.x, y: start.y}, {x: end.x, y: end.y});
+            this.drawLine(canvasID, {x: start.x, y: start.y}, {x: end.x, y: end.y}, color, width);
         }
     }
 
@@ -62,6 +79,14 @@ export default class GameCanvas extends React.Component{
         const y = this.state.gridSize * (grid.r) + this.state.gridSize/2
         return(this.Point(x,y))
     }
+    
+    pixelToGrid(position){
+        let q = Math.floor(position.x / (this.state.canvasSize.canvasWidth/(this.state.gridSize/2)));
+        if(q < 0){q = 0}
+        let r = Math.floor(position.y /(this.state.canvasSize.canvasHeight/(this.state.gridSize/4)));
+        if(r < 0){r = 0}
+        return(this.Grid(q, r))
+    }
 
     Point(x, y) {
         return{x: x, y: y}
@@ -71,20 +96,41 @@ export default class GameCanvas extends React.Component{
         return {q: q, r: r}
     }
 
-    drawLine(canvasID, start, end) {
+    drawLine(canvasID, start, end, color, width) {
         const ctx = canvasID.getContext("2d");
         ctx.beginPath();
         ctx.moveTo(start.x, start.y);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width;
         ctx.lineTo(end.x, end.y);
         ctx.stroke();
         ctx.closePath();
     }
 
+    getCanvasPosition(canvasID){
+        const rect = canvasID.getBoundingClientRect();
+        this.setState({
+            canvasPosition: {left: rect.left, right: rect.right, top: rect.top, bottom: rect.bottom}
+        })
+    }
+
+    handleMouseMove(e){
+        const {left, right, top, bottom} = this.state.canvasPosition;
+        const offsetX = e.pageX - left;
+        const offsetY = e.pageY - top;
+        const {q, r} = this.pixelToGrid({x: offsetX, y: offsetY});
+        const {x, y} = this.gridToPixel(this.Grid(q, r));
+        this.setState({currentGrid: {q, r, x, y}})
+    }
+
     render () {
         return(
             <div>
-                <canvas ref={canvasGrid => this.canvasGrid = canvasGrid}>
-                
+                <canvas ref={canvasGrid => this.canvasGrid = canvasGrid}></canvas>
+                <canvas ref={canvasCoordinates => this.canvasCoordinates = canvasCoordinates}
+                        onMouseMove = {this.handleMouseMove}
+                >
+
                 </canvas>
             </div>
 
